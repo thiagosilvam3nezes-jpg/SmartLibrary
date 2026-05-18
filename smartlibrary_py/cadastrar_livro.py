117d2a17a8d5b9916f5a45a4d4f72e6a550461ca
thiagosilva-jpg <thiago.silva.m3nezes@gmail.com>
2026-05-18 19:20:46 -0300

    Adicionando codigos no py

--- smartlibrary_py/cadastrar_livro.py
+++ smartlibrary_py/cadastrar_livro.py
@@ -1,4 +1,4 @@
-from conexao import *
+from smartlibrary_py.conexao import *
 
 print("Bem-vindo ao Sistema de adicionar livros na Biblioteca Inteligente!")
 
--- smartlibrary_py/conexao.py
+++ smartlibrary_py/conexao.py
@@ -1,24 +1,39 @@
-# em andamento
 import mysql.connector
 
-def conexao(endereco, usuario, senha, bancodedados):
+from typing import cast
+from mysql.connector import MySQLConnection
+
+
+def conexao(
+    endereco: str,
+    usuario: str,
+    senha: str,
+    bancodedados: str
+) -> MySQLConnection | None:
+
     try:
-        return mysql.connector.connect(
+        connection = mysql.connector.connect(
             host=endereco,
             user=usuario,
             password=senha,
             database=bancodedados
         )
+
+        return cast(MySQLConnection, connection)
+
     except mysql.connector.Error as err:
         print(f"Erro ao tentar se conectar ao banco de dados: {err}")
         return None
-    
-def fecharConexao(connection):
+
+
+def fecharConexao(connection: MySQLConnection) -> None:
     if connection:
         connection.close()
 
-def adicionarLivro(connection):
-    cursor = connection.cursor()
-        
-    
 
+def adicionarLivro(connection: MySQLConnection) -> None:
+    cursor = connection.cursor()
+    
+    SQL = "INSERT INTO LIVROS (TITULO) VALUES (%s)"
+    
+    cursor.execute(SQL, )
